70c5ad73c8b0d75cf20c274d6cad7546a1254c0f
shimjaedoo <woen1637@naver.com>
2024-03-12 00:16:04 +0900

    feat : Payload Interface

---
+++ src/_common/jwt/types/payload.interface.ts
@@ -0,0 +1,8 @@
+import { JwtPayload } from 'jsonwebtoken';
+
+export interface IPayload extends JwtPayload {
+  id: string;
+  email: string | null;
+  nickname: string | null;
+  profileImage: string | null;
+}
