d01233bf115ea292bd814c721cea3fb77547db5c
shimjaedoo <woen1637@naver.com>
2024-03-14 22:13:37 +0900

    feat : Naver API

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { AuthService } from './auth.service';
 import { KakaoAuthGuard } from '../_common/_utils/guards/kakao.auth.guard';
 import { Social } from '../_common/_utils/decorators/social.decorator';
 import { oAuthDto } from './types/o-auth/request.dto';
+import { NaverAuthGuard } from '../_common/_utils/guards/naver.auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -11,6 +12,12 @@ export class AuthController {
   @Get('kakao')
   @UseGuards(KakaoAuthGuard)
   async kakao(@Social() social: oAuthDto): Promise<string> {
+    return await this.authService.oAuthLogin(social);
+  }
+
+  @Get('naver')
+  @UseGuards(NaverAuthGuard)
+  async naver(@Social() social: oAuthDto): Promise<string> {
     return await this.authService.oAuthLogin(social);
   }
 }
